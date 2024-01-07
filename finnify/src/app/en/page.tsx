574690941324015f1bdd4fa60aa3e7f7f4148ff1
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-01-07 22:37:55 +0200

    logo component created.

--- finnify/src/app/en/page.tsx
+++ finnify/src/app/en/page.tsx
@@ -1,11 +1,13 @@
 import {useTranslations} from 'next-intl';
 import Image from 'next/image'
 import Menu from '@/components/menu';
+import Logo from '@/components/logo';
  
 export default function Index() {
   const t = useTranslations('Index');
   return (    
   <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <Logo/>
     <div>{t('title')}</div>
     <Menu/>
 </main>);

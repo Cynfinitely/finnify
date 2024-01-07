1f2a9085dcbd31a45328d7a5bd799b3b43376136
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-01-07 22:33:00 +0200

    menu comp created

--- finnify/src/app/en/page.tsx
+++ finnify/src/app/en/page.tsx
@@ -1,10 +1,12 @@
 import {useTranslations} from 'next-intl';
 import Image from 'next/image'
+import Menu from '@/components/menu';
  
 export default function Index() {
   const t = useTranslations('Index');
   return (    
   <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div>{t('title')}</div>
+    <Menu/>
 </main>);
 }

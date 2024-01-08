6aa402a9189be583eacf9a39f9b98ff4192f5cd4
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-01-08 21:05:16 +0200

    menu added.

--- finnify/src/components/menu.tsx
+++ finnify/src/components/menu.tsx
@@ -1,3 +1,13 @@
+import { useTranslations } from "next-intl";
+
 export default function Menu (){
-    return <div>TEST</div>
+    const t = useTranslations('Menu');
+    
+    return <div><ul>
+        <li><a href="">{t('reading')}</a></li>
+        <li><a href="">{t('writing')}</a></li>
+        <li><a href="">{t('speaking')}</a></li>
+        <li><a href="">{t('listening')}</a></li>
+        <li><a href="">{t('grammer')}</a></li>
+        </ul></div>
 }

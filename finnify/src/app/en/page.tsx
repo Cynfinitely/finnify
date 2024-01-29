cd712d1861a1a6bb9e19bf05836284a3f9c9b54f
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-01-29 20:34:13 +0200

    .

--- finnify/src/app/en/page.tsx
+++ finnify/src/app/en/page.tsx
@@ -1,19 +1,10 @@
 import {useTranslations} from 'next-intl';
-import Menu from '@/components/menu';
-import Logo from '@/components/logo';
  
 export default function Index() {
   const t = useTranslations('Index');
-  return (    
-  <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
-    <div className='flex flex-row'>
-      <Logo/>
-    </div>
-    <div className="flex flex-row items-center justify-between p-24 gap-52">
-    <Menu/>
-    <div>{t('title')}</div>
-    </div>
-
-</main>);
+  return (   
+    <>
+        <div>{t('title')}</div>
+    </> 
+);
 }
--- finnify/src/app/layout.tsx
+++ finnify/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Menu from '@/components/menu'
+import Logo from '@/components/logo';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        <body className="flex min-h-screen flex-col items-center justify-between p-24">
+    
+    <div className='flex flex-row'>
+      <Logo/>
+    </div>
+    <div className="flex flex-row items-center justify-between p-24 gap-52">
+    <Menu/>
+    <main className={inter.className}>{children}</main>
+    </div>
+    </body>
+      
     </html>
   )
 }

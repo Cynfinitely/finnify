cb5e48264add9e230b3bfd55ddf1d8f30aafd237
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-02-06 21:26:32 +0200

    style cahnges

--- pages/writing.tsx
+++ pages/writing.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const writing = () => {
   return (
     <div className="flex flex-col w-full h-full justify-center items-center">
-      <form className="flex flex-col">
+      <form className="flex flex-col border-r-4 border-[var(--thirdColor)] p-6 gap-6">
         <textarea name="" id="" />
         <input type="submit" value="Check" />
       </form>

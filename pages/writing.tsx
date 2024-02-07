8b741078e2164605b2b94a44139127e595563d55
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-02-07 22:20:27 +0200

    output message place

--- pages/writing.tsx
+++ pages/writing.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const writing = () => {
   return (
-    <div className="flex flex-col w-full h-full justify-center items-center">
-      <form className="flex flex-col border-r-4 border-[var(--thirdColor)] p-6 gap-6">
+    <div className="flex  w-full h-full justify-center items-center">
+      <form className="flex flex-col border-r-4 border-[var(--thirdColor)] p-6 gap-6 w-1/2 h-1/2">
         <textarea name="" id="" />
         <input type="submit" value="Check" />
       </form>
+      <div className="w-1/2 bg-cyan-200 h-1/2">OUTPUT MESSAGE</div>
     </div>
   );
 };

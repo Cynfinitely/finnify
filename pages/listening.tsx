653197b0f9ac374caa110ee0d499a947a33da79f
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-02-08 22:13:08 +0200

    iframe additions

--- pages/listening.tsx
+++ pages/listening.tsx
@@ -8,6 +8,16 @@ const listening = () => {
         <li>
           <a href="https://yle.fi/selkouutiset">Selkouutiset</a>
         </li>
+        <li>
+          <iframe
+            width="400"
+            height="400"
+            src="https://www.youtube.com/embed/Or0bohcTulI?list=PL632D60E299A69D9A"
+            title="Kuulostaa Hyvältä - Sounds Good 1"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </li>
       </ol>
     </div>
   );

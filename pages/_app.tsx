3673be616da0e4423cad5c38774580db76f412af
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-02-11 20:26:47 +0200

    redux created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import Header from "../components/Header";
 import AsideMenu from "../components/AsideMenu";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 
 function MyApp({
   Component,
@@ -10,17 +12,19 @@ function MyApp({
   pageProps: any;
 }) {
   return (
-    <>
-      <div className="w-screen h-screen">
-        <Header />
-        <div className="flex w-screen h-3/4">
-          <AsideMenu />
-          <div className="w-5/6 h-full">
-            <Component {...pageProps} />
+    <Provider store={store}>
+      <>
+        <div className="w-screen h-screen">
+          <Header />
+          <div className="flex w-screen h-3/4">
+            <AsideMenu />
+            <div className="w-5/6 h-full">
+              <Component {...pageProps} />
+            </div>
           </div>
         </div>
-      </div>
-    </>
+      </>
+    </Provider>
   );
 }
 
---
+++ redux/slices/authSlice.tsx
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface AuthState {
+  user: string | null;
+}
+
+const initialState: AuthState = {
+  user: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<string>) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+
+export default authSlice.reducer;

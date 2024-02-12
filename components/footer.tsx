2d5a6af85c6b89e1cb573804ea04dbca244c5ae4
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-02-12 20:57:17 +0200

    footer update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const footer = () => {
-  return <div>footer</div>;
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#f8f9fa",
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
+      <p>&copy; {new Date().getFullYear()} Finnify</p>
+      <p>
+        <a href="/about">About Us</a> | <a href="/terms">Terms of Service</a> |{" "}
+        <a href="/privacy">Privacy Policy</a>
+      </p>
+    </footer>
+  );
 };
 
-export default footer;
+export default Footer;

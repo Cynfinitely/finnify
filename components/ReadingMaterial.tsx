1707b08229f5b2c8328aa430523f7245c5efadcb
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-02-20 14:08:55 +0200

    Reading Material Component Created

---
+++ components/ReadingMaterial.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const ReadingMaterial = () => {
+  return (
+    <div>
+      <Link href="/">
+        <img src="" alt="Reading Image" />
+        <h1>Reading Title</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default ReadingMaterial;
--- pages/reading.tsx
+++ pages/reading.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import ReadingMaterial from "@/components/ReadingMaterial";
+import React from "react";
 
 const reading = () => {
   return (
-    <div className="flex flex-col w-full h-full justify-center items-center">reading</div>
-  )
-}
+    <div className="flex flex-col w-full h-full justify-center items-center">
+      <ReadingMaterial />
+    </div>
+  );
+};
 
-export default reading
+export default reading;

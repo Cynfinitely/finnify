063b81d142cead515a953d07bef1dff36e62faef
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-02-20 14:04:53 +0200

    index update with some introduction.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const index = () => {
-  return <div className="flex flex-col w-full h-full justify-center items-center">index</div>;
+  return (
+    <div className="flex flex-col w-full h-full justify-center items-center gap-4 px-16">
+      <h1 className="text-5xl font-bold">Welcome to Finnify!</h1>
+      <h3 className="font-bold">How to use this website?</h3>
+      <p>
+        There are 5 main topics of learning Finnish: Reading, Listening,
+        Speaking, Writing and Grammer. You can study daily each of them or you
+        can select one of them and improve yourself on specificic area.
+      </p>
+    </div>
+  );
 };
 
 export default index;

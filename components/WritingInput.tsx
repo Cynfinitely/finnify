812fdf3aec9c0b943e0f182528986dadd432296d
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-02-22 17:58:52 +0200

    writing input added.

---
+++ components/WritingInput.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const WritingInput = () => {
+  return (
+    <div>
+      {" "}
+      <form className="flex flex-col border-r-4 border-[var(--thirdColor)] p-6 gap-6 w-1/2 h-1/2">
+        <textarea name="" id="" />
+        <input type="submit" value="Check" />
+      </form>
+    </div>
+  );
+};
+
+export default WritingInput;
--- pages/writing.tsx
+++ pages/writing.tsx
@@ -1,12 +1,10 @@
+import WritingInput from "@/components/WritingInput";
 import React from "react";
 
 const writing = () => {
   return (
     <div className="flex  w-full h-full justify-center items-center">
-      <form className="flex flex-col border-r-4 border-[var(--thirdColor)] p-6 gap-6 w-1/2 h-1/2">
-        <textarea name="" id="" />
-        <input type="submit" value="Check" />
-      </form>
+      <WritingInput />
       <div className="w-1/2 bg-cyan-200 h-1/2">OUTPUT MESSAGE</div>
     </div>
   );

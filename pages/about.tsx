2325b3b9a23cc8ca3e84d08916bb65db69baea7f
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-02-28 22:57:07 +0200

    .

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const about = () => {
-  return <div  className="flex flex-col w-full h-full justify-center items-center">about</div>;
+  return (
+    <div className="flex flex-col w-full h-full justify-center items-center">
+      About
+    </div>
+  );
 };
 
 export default about;
